e40c4cad7db20ec1310eb8d91358b2e9b579b4d2
viet dong <levietdongqt@gmail.com>
2024-01-15 19:34:28 +0700

    folder structure

---
+++ FrontEnd/src/app/(users)/home/page.tsx
@@ -0,0 +1,9 @@
+
+export default function Page() {
+
+  return (
+    <>
+      <h1>Home in home nè</h1>
+    </>
+  )
+}
---
+++ FrontEnd/src/app/(users)/profile/page.tsx
@@ -0,0 +1,9 @@
+
+export default function Page() {
+
+  return (
+    <>
+      <h1>Profile nè</h1>
+    </>
+  )
+}
